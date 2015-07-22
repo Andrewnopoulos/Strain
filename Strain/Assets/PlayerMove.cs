41c1d2c152a2a7f72aaa4702612237013cc847f6
Andrew <andrew.giannopoulos@gmail.com>
2015-07-22 12:03:27 +1000

    Initialized project with simple movement prototype

---
+++ Strain/Assets/PlayerMove.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 velocity = new Vector2 (0, 0);
+
+		if (Input.GetKey (KeyCode.A)) {
+			velocity.x -= 1;
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			velocity.y -= 1;
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			velocity.x += 1;
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			velocity.y += 1;
+		}
+
+		transform.position = new Vector3 (velocity.x + transform.position.x, transform.position.y, velocity.y + transform.position.z);
+
+	}
+}
