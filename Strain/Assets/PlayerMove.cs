4fba00cbd590f8a5be6e280bc3de87c129923991
Breadmouth <ipspdowling@gmail.com>
2015-07-23 17:25:51 +1000

    Added simple rotation based on mouse position.

--- Strain/Assets/PlayerMove.cs
+++ Strain/Assets/PlayerMove.cs
@@ -10,24 +10,39 @@ public class PlayerMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		//Movement code
 		Vector2 velocity = new Vector2 (0, 0);
 
-		if (Input.GetKey (KeyCode.A)) {
+		if (Input.GetKey (KeyCode.A)) 
+		{
 			velocity.x -= 1;
 		}
-
-		if (Input.GetKey (KeyCode.S)) {
+		if (Input.GetKey (KeyCode.S)) 
+		{
 			velocity.y -= 1;
 		}
-		if (Input.GetKey (KeyCode.D)) {
+		if (Input.GetKey (KeyCode.D)) 
+		{
 			velocity.x += 1;
 		}
-
-		if (Input.GetKey (KeyCode.W)) {
+		if (Input.GetKey (KeyCode.W)) 
+		{
 			velocity.y += 1;
 		}
 
 		transform.position = new Vector3 (velocity.x + transform.position.x, transform.position.y, velocity.y + transform.position.z);
 
+		//Rotation code
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		float distance = 0.0f;
+		if (Physics.Raycast (ray, out hit)) 
+		{
+			distance = hit.distance;
+		}
+		transform.LookAt (ray.GetPoint (distance - 1.28f), Vector3.up);
+
+		//Camera.main.transform.position = new Vector3 (transform.position.x, transform.position.y, Camera.main.transform.position.z);
 	}
 }
