8fae9dbc1d4732544d6bc289addda06627a19261
Breadmouth <ipspdowling@gmail.com>
2015-07-24 14:39:12 +1000

    Files I forgot to add.

---
+++ Strain/Assets/CameraMove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Vector3 thing = player.transform.position;
+		transform.position = player.transform.position + new Vector3 (0.0f, 20.0f, -10.0f);
+	}
+}
---
+++ Strain/Assets/PlayerRotate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//Rotation code
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		float distance = 0.0f;
+		if (Physics.Raycast (ray, out hit)) 
+		{
+			distance = hit.distance;
+		}
+		transform.LookAt (ray.GetPoint (distance - 1.28f), Vector3.up);
+
+	}
+}
