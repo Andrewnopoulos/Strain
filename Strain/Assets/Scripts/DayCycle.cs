f81b8861988850090a0c2b44ac704444b0357384
Andrew Giannopoulos <andrew.giannopoulos@gmail.com>
2015-08-07 16:20:47 +1000

    Fixed day-night cycle

--- Strain/Assets/Scripts/DayCycle.cs
+++ Strain/Assets/Scripts/DayCycle.cs
@@ -5,9 +5,6 @@ public class DayCycle : MonoBehaviour {
 
     public Light sun;
 
-    private float maxDay = Mathf.PI * 100;
-    public float currentDay = Mathf.PI * 100 / 2;
-
 	// Use this for initialization
 	void Start () {
 
@@ -16,14 +13,12 @@ public class DayCycle : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        currentDay += Time.deltaTime;
+        sun.transform.Rotate(new Vector3(0, -1, 0), Time.deltaTime);
+        sun.intensity = Vector3.Dot(sun.transform.forward, new Vector3(0, -1, 0));
 
-        if (currentDay >= maxDay)
-            currentDay = 0;
+        //sun.intensity = Mathf.Clamp(2 * Mathf.Sin(currentDay / 100), 0.2f, 1.2f);
 
-        sun.intensity = Mathf.Clamp(2 * Mathf.Sin(currentDay / 100), 0.2f, 1.2f);
-
-        sun.transform.Rotate(new Vector3(0, 1, 0), -Mathf.PI / 4 *  Time.deltaTime);
+        //sun.transform.Rotate(new Vector3(0, 1, 0), -Mathf.PI / 4 *  Time.deltaTime);
 
 	}
 }
